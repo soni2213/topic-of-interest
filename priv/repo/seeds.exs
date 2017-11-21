56ef30c6440eba34023183429dd959d87173572d
soni sahu <soni@grassious.com>
2017-11-21 23:23:11 +0530

    add seeds for topics of interests

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -5,7 +5,20 @@
 # Inside the script, you can read and write to any of your
 # repositories directly:
 #
-#     ProjectOne.Repo.insert!(%ProjectOne.SomeModel{})
+#    ProjectOne.Repo.insert!(%ProjectOne.SomeModel{})
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will halt execution if something goes wrong.
+
+Enum.each ["Photography", "Space", "Sports",
+            "Cars", "Rock Music", "Dance",
+            "Chess", "Jumba", "Singing",
+            "Reading", "Ticket Collection", "TV Shows",
+            "Painting", "Skating", "Acting",
+            "Poetry", "Pottery", "Cooking",
+            "Yoga", "Video Games"], fn name ->
+  ProjectOne.Repo.insert!(%ProjectOne.TopicOfInterest{name: name, short_desc: ""})
+end
+
+
+# ProjectOne.Repo.insert!(%ProjectOne.TopicOfInterest{name: "", short_desc: ""})
